643790ae18e81e0cff9cf2ddc1cd0924b67d08a4
TheBalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-06-09 18:07:24 +0200

    Update Main.php

--- src/TheBalkanDev/Main.php
+++ src/TheBalkanDev/Main.php
@@ -8,6 +8,7 @@ use pockermine\utils\Utils;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\level\sound\PopSound;
 use pocketmine\level\particle\LavaParticle;
 use pocketmine\item\Item;
 
@@ -23,31 +24,31 @@ class Main extends PluginBase implements Listener{
 	public function onBreak(BlockBreakEvent $e){
 		$p = $e->getPlayer();
 		$b = $e->getBlock();
-		if ($p->isSurvival()){}
 		if($e->getBlock()->getId() == 1 && mt_rand(0,$this->getConfig()->get("lvl1-chance")) == "1"){
 			for($i = 0; $i <= 5; $i++){
 				$p->getLevel()->addParticle(new LavaParticle($b, 2));
 			}
+			$p->getLevel()->addSound(new PopSound($p));
 			$p->sendMessage($this->getConfig()->get("lvl1-message"));
 			foreach($this->getConfig()->get("lvl1-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl1-item-max"))));
 			}
 		}
-		if ($p->isSurvival()){}
 		if($e->getBlock()->getId() == 1 && mt_rand(0,$this->getConfig()->get("lvl2-chance")) == "1"){
 			for($i = 0; $i <= 8; $i++){
 				$p->getLevel()->addParticle(new LavaParticle($b, 2));
 			}
+			$p->getLevel()->addSound(new PopSound($p));
 			$p->sendMessage($this->getConfig()->get("lvl2-message"));
 			foreach($this->getConfig()->get("lvl2-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl2-item-max"))));
 			}
 		}
-		if ($p->isSurvival()){}
 		if($e->getBlock()->getId() == 1 && mt_rand(0,$this->getConfig()->get("lvl3-chance")) == "1"){
 			for($i = 0; $i <= 11; $i++){
 				$p->getLevel()->addParticle(new LavaParticle($b, 2));
 			}
+			$p->getLevel()->addSound(new PopSound($p));
 			$p->sendMessage($this->getConfig()->get("lvl3-message"));
 			foreach($this->getConfig()->get("lvl3-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl3-item-max"))));
