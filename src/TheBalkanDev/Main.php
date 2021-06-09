3fb2e8778d0574abdaeb5bb73f9173db2ee2a6c3
TheBalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-06-09 15:23:01 +0200

    Update Main.php

--- src/TheBalkanDev/Main.php
+++ src/TheBalkanDev/Main.php
@@ -29,7 +29,7 @@ class Main extends PluginBase implements Listener{
 				$p->getLevel()->addParticle(new LavaParticle($b, 2));
 			}
 			$p->getLevel()->addSound(new PopSound($p));
-			$p->sendMessage($this->getConfig()->get("lvl1-message"));
+			$this->sendMessage($this->getConfig()->get("lvl1-message"));
 			foreach($this->getConfig()->get("lvl1-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl1-item-max"))));
 			}
@@ -39,7 +39,7 @@ class Main extends PluginBase implements Listener{
 				$p->getLevel()->addParticle(new LavaParticle($b, 2));
 			}
 			$p->getLevel()->addSound(new PopSound($p));
-			$p->sendMessage($this->getConfig()->get("lvl2-message"));
+			$this->sendMessage($this->getConfig()->get("lvl2-message"));
 			foreach($this->getConfig()->get("lvl2-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl2-item-max"))));
 			}
@@ -49,7 +49,7 @@ class Main extends PluginBase implements Listener{
 				$p->getLevel()->addParticle(new LavaParticle($b, 2));
 			}
 			$p->getLevel()->addSound(new PopSound($p));
-			$p->sendMessage($this->getConfig()->get("lvl3-message"));
+			$this->sendMessage($this->getConfig()->get("lvl3-message"));
 			foreach($this->getConfig()->get("t3-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl3-item-max"))));
 			}
