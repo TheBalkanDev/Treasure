b635c005f55b5e29b95f153ea942ac1735d027ea
TheBalkanDev <75144786+TheBalkanDev@users.noreply.github.com>
2021-06-10 21:15:03 +0200

    Fix all!

--- src/TheBalkanDev/Main.php
+++ src/TheBalkanDev/Main.php
@@ -3,7 +3,6 @@
 namespace TheBalkanDev;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
 use pocketmine\Server;
 use pockermine\utils\Utils;
 use pocketmine\event\Listener;
@@ -19,7 +18,7 @@ class Main extends PluginBase implements Listener{
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		$this->saveDefaultConfig();
 		
-		$this->getServer()->getLogger()->info(TextFormat::GREEN."[Treasure] is now on!");
+		$this->getServer()->getLogger()->info(TextFormat::GREEN."[Treasure] Active!");
 	}
 
 	public function onBreak(BlockBreakEvent $e){
@@ -27,8 +26,9 @@ class Main extends PluginBase implements Listener{
 		$b = $e->getBlock();
 		if($e->getBlock()->getId() == 1 && mt_rand(0,$this->getConfig()->get("lvl1-chance")) == "1"){
 			for($i = 0; $i <= 5; $i++){
-		        $p->getLevel()->addParticle(new LavaParticle($b, 2));
-			$p->addSound(new PopSound($p));
+				$p->getLevel()->addParticle(new LavaParticle($b, 2));
+			}
+			$p->getLevel()->addSound(new PopSound($p));
 			$p->sendMessage($this->getConfig()->get("lvl1-message"));
 			foreach($this->getConfig()->get("lvl1-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl1-item-max"))));
@@ -36,8 +36,9 @@ class Main extends PluginBase implements Listener{
 		}
 		if($e->getBlock()->getId() == 1 && mt_rand(0,$this->getConfig()->get("lvl2-chance")) == "1"){
 			for($i = 0; $i <= 8; $i++){
-		        $p->getLevel()->addParticle(new LavaParticle($b, 2));
-			$p->addSound(new PopSound($p));
+				$p->getLevel()->addParticle(new LavaParticle($b, 2));
+			}
+			$p->getLevel()->addSound(new PopSound($p));
 			$p->sendMessage($this->getConfig()->get("lvl2-message"));
 			foreach($this->getConfig()->get("lvl2-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl2-item-max"))));
@@ -45,12 +46,13 @@ class Main extends PluginBase implements Listener{
 		}
 		if($e->getBlock()->getId() == 1 && mt_rand(0,$this->getConfig()->get("lvl3-chance")) == "1"){
 			for($i = 0; $i <= 11; $i++){
-		        $p->getLevel()->addParticle(new LavaParticle($b, 2));
-			$p->addSound(new PopSound($p));
+				$p->getLevel()->addParticle(new LavaParticle($b, 2));
+			}
+			$p->getLevel()->addSound(new PopSound($p));
 			$p->sendMessage($this->getConfig()->get("lvl3-message"));
 			foreach($this->getConfig()->get("lvl3-loot") as $loot){
 				$p->getInventory()->addItem(Item::get($loot,0,mt_rand(0,$this->getConfig()->get("lvl3-item-max"))));
 			}
 		}
 	}
-}			
+}
